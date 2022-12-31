bbd99ab492f1499a4d9360bdcbc7812415ec66be
JumjieYang <junjie@jyang.dev>
2022-12-31 14:17:12 -0500

    Import client
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+const App = () => {
+  return (
+    <div>
+      <button className="p-2 rounded bg-blue-500 hover:bg-blue-600 transition">
+       Click ME 
+      </button>
+    </div>
+  );
+};
+
+export default App;
