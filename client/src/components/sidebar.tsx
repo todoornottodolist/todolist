c842ddb8c22fbbe22a1acfa7ffca03d038f7c30c
Xiangyi Li <venusli370306@gmail.com>
2023-01-09 04:48:34 +0800

    Remove extra packages

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -1,12 +1,27 @@
 import React from "react"
+import trailSignOutLineSvg from '../assets/trail-sign-outline.svg'
+
 
 export const SideBar = () => {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 
                     flex flex-col
                     text-white shadow-lg bg-white">
-        <i></i>
+        <SideBarIcon icon={<img src={trailSignOutLineSvg} />} />
     </div>
   )
 }
 
+const Avatar = () => {
+  return (
+    <div>
+      <img src="./assets/"/>
+    </div>
+  )
+}
+
+const SideBarIcon = ({ icon }) => (
+  <button className="sidebar-icon group">
+    {icon}
+  </button>
+);
